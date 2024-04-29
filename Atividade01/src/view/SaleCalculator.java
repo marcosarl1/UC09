b9a71a375bac4d35c2e380c9d4f58b96a2b524fb
Marcos Júnior <marcosajr11@gmail.com>
2024-04-28 21:02:14 -0300

    Improved things

--- Atividade01/src/view/SaleCalculator.java
+++ Atividade01/src/view/SaleCalculator.java
@@ -1,6 +1,5 @@
 package view;
 
-import com.formdev.flatlaf.FlatClientProperties;
 import java.awt.Font;
 import javax.swing.JButton;
 import javax.swing.JLabel;
@@ -8,25 +7,28 @@ import javax.swing.JPanel;
 import javax.swing.JTextField;
 import net.miginfocom.swing.MigLayout;
 import java.awt.Color;
-
-import controller.SaleController;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+import model.entities.Sale;
 
 public class SaleCalculator extends JFrame {
 
-    private JTextField valueField;
+    private JTextField txtValue;
     private JButton bttnCalc;
+
+    private static final Double MIN_PRICE_FOR_DISCOUNT = 500.00;
 
     public SaleCalculator() {
         init();
         setTitle("Calculadora de Venda");
-        setSize(300,300);
+        setSize(300, 300);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
 
     public JTextField getValueField() {
-        return valueField;
+        return txtValue;
     }
 
     private void init() {
@@ -35,25 +37,23 @@ public class SaleCalculator extends JFrame {
 
         JLabel lblTitle = new JLabel("Calcular venda");
         lblTitle.setFont(new Font("Roboto", Font.BOLD, 24));
-        
-        valueField = new JTextField();
-        valueField.putClientProperty(FlatClientProperties.PLACEHOLDER_TEXT, "Insira o valor da venda"); //Utilizando FlatLaf para adicionar texto PlaceHolder aumentar acessibilidade do programa.
-        valueField.setToolTipText("Insira o valor da venda");
-        
+
+        txtValue = new JTextField();
+        txtValue.setToolTipText("Insira o valor da venda");
+
         JLabel hintLabel = new JLabel("Use '.' para casas decimais (ex: 99.99)");
         hintLabel.setFont(new Font(hintLabel.getName(), Font.PLAIN, 10));
-        
 
         //Adicionando ao panel
         add(lblTitle, "gapbottom 45, wrap");
         add(new JLabel("Valor da venda (R$)"), "wrap");
-        add(valueField, "growx, wrap");
+        add(txtValue, "growx, wrap");
         add(hintLabel, "gapy 0, span");
 
         //Criando label para que o botão possa ficar no canto inferior direito da tela
         JPanel bttnPanel = new JPanel(new MigLayout("wrap, insets 20", "[right]"));
         bttnCalc = new JButton("Calcular");
-        bttnCalc.setBackground(new Color(0,122,255));
+        bttnCalc.setBackground(new Color(0, 122, 255));
         bttnCalc.setForeground(Color.WHITE);
         bttnCalc.setFont(new Font(bttnCalc.getName(), Font.BOLD, 12));
 
@@ -63,8 +63,44 @@ public class SaleCalculator extends JFrame {
 
         //Adicionando Listener para botão calcular
         bttnCalc.addActionListener(l -> {
-            SaleController.calculateSale(this);
-            valueField.setText("");
+            calculateSale();
+            txtValue.setText("");
         });
+    }
+
+    private void calculateSale() {
+        try {
+            double price = Double.parseDouble(txtValue.getText());
+            if (price <= 0) {
+                throw new IllegalArgumentException("O valor da venda deve ser maior que zero.");
+            }
+            if (price > MIN_PRICE_FOR_DISCOUNT) {
+                Sale sale = new Sale(price);
+                String discountInput = JOptionPane.showInputDialog(null, "Insira a porcentagem de desconto.",
+                        "Solicitar desconto", JOptionPane.DEFAULT_OPTION);
+                if (discountInput == null) { // Caso usuário feche popup de requisição de porcentagem, voltar para tela inicial
+                    return;
+                }
+                double discount = Double.parseDouble(discountInput);
+                sale.requestDiscount(discount); // Chamando método da lógica de desconto da classe Sale
+                JOptionPane.showMessageDialog(null,
+                        String.format("Preço original: R$%.2f %nPreço com desconto: R$%.2f", price, sale.getPrice()),
+                        "Detalhes da venda", JOptionPane.INFORMATION_MESSAGE);
+            } else {
+                JOptionPane.showMessageDialog(null,
+                        String.format("Preço total: R$%.2f", price),
+                        "Detalhes da venda", JOptionPane.INFORMATION_MESSAGE);
+            }
+        } catch (NumberFormatException e) { // Erro para campo vazio ou com input diferente de formato com ponto flutante (divisor ".")
+            if (txtValue.getText().isEmpty()) {
+                JOptionPane.showMessageDialog(null, "Insira o valor da venda!", "Erro", JOptionPane.ERROR_MESSAGE);
+            } else {
+                JOptionPane.showMessageDialog(null, "Insira um número válido!", "Erro", JOptionPane.ERROR_MESSAGE);
+            }
+        } catch (IllegalArgumentException e) { // Erro para lidar com valores negativos e porcentagem menor que zero ou maior que cem
+            JOptionPane.showMessageDialog(null, e.getMessage(), "Erro", JOptionPane.ERROR_MESSAGE);
+        } catch (RuntimeException e) { // Erros inesperados em tempo de execução
+            JOptionPane.showMessageDialog(null, "Erro inesperado! Tente novamente", "Erro", JOptionPane.ERROR_MESSAGE);
+        }
     }
 }
